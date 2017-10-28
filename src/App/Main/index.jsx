39b3bd2b55f617da2160b8767cc1b666e8afd008
Slendimon <pdassem@gmail.com>
2017-10-28 17:21:17 -0500

    login in react

--- src/App/Main/index.jsx
+++ src/App/Main/index.jsx
@@ -1,11 +1,12 @@
 import { Switch, Route } from "react-router-dom";
 import React from "react";
 import Home from "../../Components/Home/index.jsx";
-
+import Login from "../../Components/Login/index.jsx"
 const Main = () => (
     <main>
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Home}/> 
       </Switch>
     </main>
 );
