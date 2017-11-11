e6f62c91228e40d3a40b13c220212f7d79a918ff
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2017-11-11 18:55:41 -0500

    avance del formulario de nuevo producto

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const webpack = require("webpack");
 const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const BUILD_DIR = path.resolve(__dirname, "public");
 const APP_DIR = path.resolve(__dirname, "src");
 
 const config = {
-  entry: `${APP_DIR}/index.jsx`,
+  entry: [`${APP_DIR}/index.jsx`, `${APP_DIR}/main.scss`],
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
@@ -16,9 +17,20 @@ const config = {
         test : /\.jsx?/,
         include : APP_DIR,
         loader : 'babel-loader'
+      },
+      {
+        test : /\.scss$/,
+        include : APP_DIR,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'bundle.css',
+      allChunks: true,
+    }),
+  ],
 };
 
 module.exports = config;
